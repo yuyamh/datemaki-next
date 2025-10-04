d01e5034020f0a7f5828ef8912d85d2690e175ba
yuyamh <yuya.htr0828@gmail.com>
2025-10-04 19:28:02 +0900

    Prettier を設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 function Page() {
-  return (
-    <main>
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold">Hello World</h1>
-        </div>
-      </div>
-    </main>
-  );
-};
+    return (
+        <main>
+            <div className="flex h-screen items-center justify-center">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-2xl font-bold">Hello World</h1>
+                </div>
+            </div>
+        </main>
+    );
+}
 
 export default Page;
