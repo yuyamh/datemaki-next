4c895e687dd6d232da6366898297f19b937187da
yuyamh <yuya.htr0828@gmail.com>
2025-10-13 17:19:02 +0900

    shadcn/ui＆Lucideの追加

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,60 @@
+import type { VariantProps } from "class-variance-authority";
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+import { cva } from "class-variance-authority";
+
+const buttonVariants = cva(
+    "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    {
+        defaultVariants: {
+            size: "default",
+            variant: "default",
+        },
+        variants: {
+            size: {
+                default: "h-9 px-4 py-2 has-[>svg]:px-3",
+                icon: "size-9",
+                "icon-lg": "size-10",
+                "icon-sm": "size-8",
+                lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+                sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
+            },
+            variant: {
+                default:
+                    "bg-primary text-primary-foreground hover:bg-primary/90",
+                destructive:
+                    "bg-destructive hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white",
+                ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+                link: "text-primary underline-offset-4 hover:underline",
+                outline:
+                    "bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border shadow-xs",
+                secondary:
+                    "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+            },
+        },
+    },
+);
+
+function Button({
+    asChild = false,
+    className,
+    size,
+    variant,
+    ...props
+}: React.ComponentProps<"button"> &
+    VariantProps<typeof buttonVariants> & {
+        asChild?: boolean;
+    }) {
+    const Comp = asChild ? Slot : "button";
+
+    return (
+        <Comp
+            className={cn(buttonVariants({ className, size, variant }))}
+            data-slot="button"
+            {...props}
+        />
+    );
+}
+
+export { Button, buttonVariants };
---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import type { ToasterProps } from "sonner";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+
+const Toaster = ({ ...props }: ToasterProps) => {
+    const { theme = "system" } = useTheme();
+
+    return (
+        <Sonner
+            className="toaster group"
+            style={
+                {
+                    "--normal-bg": "var(--popover)",
+                    "--normal-border": "var(--border)",
+                    "--normal-text": "var(--popover-foreground)",
+                } as React.CSSProperties
+            }
+            theme={theme as ToasterProps["theme"]}
+            {...props}
+        />
+    );
+};
+
+export { Toaster };
