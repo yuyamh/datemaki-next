2bfb2e5eea4e2acd0bff351ecd45062710a04ec0
yuyamh <yuya.htr0828@gmail.com>
2026-04-27 22:39:48 +0900

    [fix]投稿ボタンの大きさを変更

--- src/app/ui/fab-create-post.tsx
+++ src/app/ui/fab-create-post.tsx
@@ -7,7 +7,7 @@ import { PencilLine } from "lucide-react";
 export function FabCreate() {
     return (
         <Button
-            className="fixed right-8 bottom-8 z-50 h-16 w-16 rounded-full shadow-lg hover:bg-amber-300"
+            className="fixed right-8 bottom-8 z-50 h-18 w-18 rounded-full shadow-lg hover:bg-amber-300"
             onClick={() => {
                 redirect("/posts/new");
             }}
